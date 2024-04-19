15ba1b1726cdba0f6c701a5376ac318f5de7b5c1
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2024-04-19 17:32:29 +0200

    new test2

--- controllers/test2Controller.js
+++ controllers/test2Controller.js
@@ -1,5 +1,14 @@
 // controllers/test2Controller.js
-
+import axios from 'axios';
 export function getTest2(req, res) {
-    res.send('Hello World from service /test2');
+    //axios get to jsonplaceholder
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        console.log("GET /test2 response: 200 OK");
+        res.send(response.data);
+    })
+    .catch(error => {
+        console.error("GET /test2 response: 500 Internal Server Error");
+        res.status(500 ).send(error);
+    });
 }
